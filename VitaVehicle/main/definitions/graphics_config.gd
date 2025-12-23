bcb7ad0102f5d1693c3b949a282e0344442a064b
tomsterBG <tomi4443321@gmail.com>
2025-12-23 15:22:49 +0200

    Config definitions

---
+++ VitaVehicle/main/definitions/graphics_config.gd
@@ -0,0 +1,9 @@
+## A definition for graphics settings.
+
+class_name GraphicsConfig extends Resource
+
+
+
+@export var shadows := false
+@export var tire_smoke := false
+@export var full_screen := false
---
+++ VitaVehicle/main/definitions/user_config.gd
@@ -0,0 +1,7 @@
+## A definition for user settings.
+
+class_name UserConfig extends Resource
+
+
+
+@export var graphics := GraphicsConfig.new()
